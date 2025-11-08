827d166bad719b9f99bb1bd25cab04bfb418228a
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-11-08 17:16:15 +0200

    New: Bump .NET to 10

--- src/Prowlarr.Api.V1/Config/CertificateValidator.cs
+++ src/Prowlarr.Api.V1/Config/CertificateValidator.cs
@@ -35,7 +35,7 @@ namespace Prowlarr.Api.V1.Config
 
             try
             {
-                new X509Certificate2(resource.SslCertPath, resource.SslCertPassword, X509KeyStorageFlags.DefaultKeySet);
+                X509CertificateLoader.LoadPkcs12FromFile(resource.SslCertPath, resource.SslCertPassword, X509KeyStorageFlags.DefaultKeySet);
 
                 return true;
             }
