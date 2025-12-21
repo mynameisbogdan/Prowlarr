43285b0ae39bf5469cc8eb654f601ad03cc0ab96
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-12-21 03:29:44 +0200

    Bump FluentMigrator to 7.2
    (cherry picked from commit bb090a7c425970cba26b3d9613c13d2cbc66925f)

--- src/NzbDrone.Core/Datastore/Migration/007_history_failed.cs
+++ src/NzbDrone.Core/Datastore/Migration/007_history_failed.cs
@@ -12,7 +12,7 @@ namespace NzbDrone.Core.Datastore.Migration
                 .AddColumn("Successful").AsBoolean().NotNullable().WithDefaultValue(true);
 
             // Postgres added after this, not needed
-            IfDatabase(ProcessorId.SQLite).Execute.Sql("UPDATE \"History\" SET \"Successful\" = (json_extract(\"History\".\"Data\",'$.successful') == 'True' );");
+            IfDatabase(ProcessorIdConstants.SQLite).Execute.Sql("UPDATE \"History\" SET \"Successful\" = (json_extract(\"History\".\"Data\",'$.successful') == 'True' );");
         }
     }
 }
