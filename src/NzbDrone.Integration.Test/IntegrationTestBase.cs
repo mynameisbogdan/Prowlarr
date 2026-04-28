544a591625590f5a4d18ff290be6519791176238
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-04-28 23:03:21 +0300

    Enable Call async methods when in an async method
    (cherry picked from commit c52f5a3b11a992bd832137398fb221bb543e04ba)

--- src/NzbDrone.Integration.Test/IntegrationTestBase.cs
+++ src/NzbDrone.Integration.Test/IntegrationTestBase.cs
@@ -150,10 +150,9 @@ namespace NzbDrone.Integration.Test
 
             var cts = new CancellationTokenSource();
 
-            _signalrConnection.Closed += e =>
+            _signalrConnection.Closed += async _ =>
             {
-                cts.Cancel();
-                return Task.CompletedTask;
+                await cts.CancelAsync();
             };
 
             _signalrConnection.On<SignalRMessage>("receiveMessage", (message) =>
@@ -170,7 +169,7 @@ namespace NzbDrone.Integration.Test
                 {
                     Console.WriteLine("Connecting to signalR");
 
-                    await _signalrConnection.StartAsync();
+                    await _signalrConnection.StartAsync(cts.Token);
                     connected = true;
                     break;
                 }
@@ -183,7 +182,7 @@ namespace NzbDrone.Integration.Test
                 }
 
                 retryCount++;
-                Thread.Sleep(200);
+                await Task.Delay(200, cts.Token);
             }
         }
 
